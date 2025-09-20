8048a6d3d77e5a48cfcb37b346fc0f14290e7b69
Luriahc <faqih.chairul08@gmail.com>
2025-09-20 11:49:35 +0800

    ubah transaction 03

--- transactions/03_failure_recovery.sql
+++ transactions/03_failure_recovery.sql
@@ -3,11 +3,11 @@ USE cleaning_db;
 -- Simulasi FAILURE & RECOVERY dengan ROLLBACK
 
 START TRANSACTION;
-INSERT INTO laporan_penggantian_tissue (admin_id, pegawai_id, dispenser_id, jumlah, keterangan)
+INSERT INTO laporan_penggantian (admin_id, pegawai_id, dispenser_id, jumlah, keterangan)
 VALUES (1, 1, 1, 10, 'Simulasi gagal');
 
 -- Anggap ada error di sini → maka rollback
 ROLLBACK;
 
 -- Cek hasil (seharusnya data gagal tidak masuk)
-SELECT * FROM laporan_penggantian_tissue WHERE keterangan = 'Simulasi gagal';
+SELECT * FROM laporan_penggantian WHERE keterangan = 'Simulasi gagal';
