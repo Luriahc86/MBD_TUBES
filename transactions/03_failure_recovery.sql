89922cd6a2978f6da5f9ad678916bbcf6902aa39
Luriahc <faqih.chairul08@gmail.com>
2025-09-29 03:06:25 +0800

    update dikit

--- transactions/03_failure_recovery.sql
+++ transactions/03_failure_recovery.sql
@@ -1,14 +1,14 @@
 USE cleaning_db;
-SELECT * FROM laporan_penggantian ORDER BY id DESC LIMIT 5;
+SELECT * FROM v_laporan_penggantian ORDER BY id DESC LIMIT 5;
 
 -- Simulasi FAILURE & RECOVERY dengan ROLLBACK
 START TRANSACTION;
 
-INSERT INTO laporan_penggantian (pegawai_id, dispenser_id, jumlah, tanggal)
+INSERT INTO v_laporan_penggantian (pegawai_id, dispenser_id, jumlah, tanggal)
 VALUES (1, 1, 10, '2025-09-20');
 
 -- Anggap ada error di sini → maka rollback
 ROLLBACK;
 
 -- Cek hasil (seharusnya data gagal tidak masuk)
-SELECT * FROM laporan_penggantian WHERE jumlah = 10 AND tanggal = '2025-09-20';
+SELECT * FROM v_laporan_penggantian WHERE jumlah = 10 AND tanggal = '2025-09-20';
